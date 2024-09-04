31222798864f0df394e3da8a66b50ed4f7b9d955
mohamedfathi68 <mohamedfathielqastawy@gmail.com>
2024-09-04 03:01:41 +0300

    Database Configration

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import express from 'express'
 const app = express()
 const port =  3000
+import { car, rental, user } from './src/database/dbConnection.js'
 import cors from "cors";
 import bootstrap from './src/modules/bootstrap.routes.js';
+
 
 app.use(cors())
 app.use(express.json())
---
+++ src/database/dbConnection.js
@@ -0,0 +1,15 @@
+import { MongoClient } from "mongodb";
+
+const client = new MongoClient("mongodb://localhost:27017");
+
+client
+  .connect()
+  .then(console.log("database connected successfully"))
+  .catch(console.error);
+
+const db = client.db('CarRentalSystem');
+const car = db.collection("car");
+const rental = db.collection("rental");
+const user = db.collection("user");
+
+export { car, rental, user };
