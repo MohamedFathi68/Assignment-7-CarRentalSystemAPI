4fa5ff552e2ad12b48e79c08211cde1bdbcfda4a
mohamedfathi68 <mohamedfathielqastawy@gmail.com>
2024-09-05 01:33:37 +0300

    finish car module end points

--- src/modules/car/car.controller.js
+++ src/modules/car/car.controller.js
@@ -1,18 +1,29 @@
-const addCar = (req, res) => {
-  res.status(200).json({ message: "success" });
+import { ObjectId } from "mongodb";
+import { car } from "../../database/dbConnection.js";
+
+const addCar = async (req, res) => {
+  let data = await car.insertOne(req.body);
+  res.status(201).json({ message: "success" , data});
 };
 
-const getCarById = (req, res) => {
-  res.status(200).json({ message: "success" });
+const getCarById = async (req, res) => {
+  let data = await car.findOne({ _id: new ObjectId(req.params.id) });
+  res.status(200).json({ message: "success" , data });
 };
-const getAllCars = (req, res) => {
-  res.status(200).json({ message: "success" });
+
+const getAllCars = async (req, res) => {
+  let data = await car.find().toArray();
+  res.status(200).json({ message: "success" , data});
 };
-const updateCar = (req, res) => {
-  res.status(200).json({ message: "success" });
+
+const updateCar = async (req, res) => {
+  let data = await car.updateOne({ _id: new ObjectId(req.params.id) }, { $set: req.body });
+  res.status(200).json({ message: "success" , data});
 };
-const deleteCar = (req, res) => {
-  res.status(200).json({ message: "success" });
+
+const deleteCar = async (req, res) => {
+  let data = await car.deleteOne({ _id: new ObjectId(req.params.id) });
+  res.status(200).json({ message: "success" , data});
 };
 
 export { addCar, getCarById, getAllCars, updateCar, deleteCar };
